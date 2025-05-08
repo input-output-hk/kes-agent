8197543d5a36adc2625dbb39731ddd67f81613b0
perturbing <thomas.vellekoop@iohk.io>
2025-05-08 17:00:54 +0200

    add kes-{agent/agents-control/service-client-demo} to the runtime of the test, add cli completion

--- nix/project.nix
+++ nix/project.nix
@@ -19,13 +19,50 @@ let
       name = "my-project";
 
       compiler-nix-name = lib.mkDefault "ghc96";
-
       modules = [
-        ({ pkgs, lib, ... }: {
-          packages.kes-agent.components.library.build-tools =
-            lib.mkForce [ pkgs.git ];
-        })
+        ({ pkgs, lib, config, ... }:
+          let
+            postInstallCompletion = exeName:
+              "  BASH_COMPLETIONS=$out/share/bash-completion/completions\n  ZSH_COMPLETIONS=$out/share/zsh/site-functions\n  mkdir -p $BASH_COMPLETIONS $ZSH_COMPLETIONS\n  $out/bin/${exeName} --bash-completion-script ${exeName} > $BASH_COMPLETIONS/${exeName}\n  $out/bin/${exeName} --zsh-completion-script ${exeName} > $ZSH_COMPLETIONS/_${exeName}\n";
+          in lib.mkIf (!pkgs.stdenv.hostPlatform.isWindows) {
+            packages.kes-agent.components.library.build-tools =
+              lib.mkForce [ pkgs.git ];
+
+            # Wrap the test binary to have CLI tools in PATH
+            packages.kes-agent.components.tests.kes-agent-tests.postInstall =
+              "  wrapProgram $out/bin/kes-agent-tests --set PATH ${
+                  lib.makeBinPath [
+                    config.hsPkgs.kes-agent.components.exes.kes-agent
+                    config.hsPkgs.kes-agent.components.exes.kes-agent-control
+                    config.hsPkgs.kes-agent.components.exes.kes-service-client-demo
+                  ]
+                }\n";
+
+            # Add completions for all CLI tools
+            packages.kes-agent.components.exes.kes-agent.postInstall =
+              postInstallCompletion "kes-agent";
+            packages.kes-agent.components.exes.kes-agent-control.postInstall =
+              postInstallCompletion "kes-agent-control";
+            packages.kes-agent.components.exes.kes-service-client-demo.postInstall =
+              postInstallCompletion "kes-service-client-demo";
+          })
       ];
+
+      # modules = [
+      #   ({ pkgs, lib, config, ... }: {
+      #     packages.kes-agent.components.library.build-tools =
+      #       lib.mkForce [ pkgs.git ];
+      #     packages.kes-agent.components.tests.kes-agent-tests.postInstall = ''
+      #       wrapProgram $out/bin/kes-agent-tests --set PATH ${
+      #         lib.makeBinPath
+      #         [
+      #           config.hsPkgs.kes-agent.components.exes.kes-agent
+      #           config.hsPkgs.kes-agent.components.exes.kes-agent-control
+      #         ]
+      #       }
+      #     '';
+      #   })
+      # ];
     });
 
   cabalProject = cabalProject'.appendOverlays [ ];
