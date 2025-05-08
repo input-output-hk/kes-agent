094dc26189f3aa6b99ee0817e09d655db9f48a69
perturbing <thomas.vellekoop@iohk.io>
2025-05-08 11:14:15 +0200

    add initial flake, test do now build yet via nix

---
+++ nix/shell.nix
@@ -0,0 +1,77 @@
+# Docs for this file: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellprojectinshellargs
+# See `shellArgs` in `mkHaskellProject` in ./project.nix for more details.
+
+{ repoRoot, inputs, pkgs, lib, system }:
+
+# Each flake variant defined in your project.nix project will yield a separate
+# shell. If no flake variants are defined, then cabalProject is the original 
+# project.
+cabalProject:
+
+{
+  name = "nix-shell";
+
+  # prompt = null;
+
+  # welcomeMessage = null;
+
+  packages = [ pkgs.git ];
+
+  # scripts = {
+  #   foo = {
+  #      description = "";
+  #      group = "general";
+  #      enabled = true;
+  #      exec = ''
+  #        echo "Hello, World!"
+  #      '';
+  #    };
+  # };
+
+  # env = {
+  #   KEY = "VALUE";
+  # };
+
+  # shellHook = "";
+
+  tools = {
+    # haskellCompilerVersion = cabalProject.args.compiler-nix-name;
+    # cabal-fmt = null;
+    # cabal-install = null;
+    # haskell-language-server = null;
+    # haskell-language-server-wrapper = null;
+    # fourmolu = null;
+    # hlint = null;
+    # stylish-haskell = null;
+    # ghcid = null;
+    # shellcheck = null;
+    # prettier = null;
+    # editorconfig-checker = null;
+    # nixfmt-classic = null;
+    # optipng = null;
+    # purs-tidy = null;
+  };
+
+  preCommit = {
+    cabal-fmt.enable = true;
+    #   cabal-fmt.extraOptions = "";
+    stylish-haskell.enable = true;
+    #   stylish-haskell.extraOptions = "";
+    fourmolu.enable = true;
+    #   fourmolu.extraOptions = "";
+    hlint.enable = true;
+    #   hlint.extraOptions = "";
+    shellcheck.enable = true;
+    #   shellcheck.extraOptions = "";
+    prettier.enable = true;
+    #   prettier.extraOptions = "";
+    #   editorconfig-checker.enable = false;
+    #   editorconfig-checker.extraOptions = "";
+    nixfmt-classic.enable = true;
+    #   nixfmt-classic.extraOptions = "";
+    #   optipng.enable = false;
+    #   optipng.extraOptions = "";
+    #   purs-tidy.enable = false;
+    #   purs-tidy.extraOptions = "";
+  };
+}
