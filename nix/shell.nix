2be5815e480cca2c32d65483b3130f20a7662104
perturbing <thomas.vellekoop@iohk.io>
2025-09-24 15:40:12 +0200

    EndToEnd tests: add withShortTmp helper to keep socket paths under macOS limit

--- nix/shell.nix
+++ nix/shell.nix
@@ -27,17 +27,17 @@ let
         package = tools.cabal-fmt;
       };
       stylish-haskell = {
-        enable = true;
+        enable = false;
         package = tools.stylish-haskell;
         args = [ "--config" ".stylish-haskell.yaml" ];
       };
       fourmolu = {
-        enable = true;
+        enable = false;
         package = tools.fourmolu;
         args = [ "--mode" "inplace" ];
       };
       hlint = {
-        enable = true;
+        enable = false;
         package = tools.hlint;
         args = [ "--hint" ".hlint.yaml" ];
       };
